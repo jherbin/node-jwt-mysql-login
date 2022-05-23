27e90ed77fae6c972be99af0afecdb026753ec5e
jherbin <jacek.herbin@gmail.com>
2022-05-23 08:22:42 +0200

    Update router.js

--- src/MySQL/routes/router.js
+++ src/MySQL/routes/router.js
@@ -18,3 +18,45 @@ router.get('/secret-route', (req, res, next) => {
 });
 
 module.exports = router;
+
+router.post('/sign-up', userMiddleware.validateRegister, (req, res, next) => {
+  db.query(
+    `SELECT * FROM users WHERE LOWER(username) = LOWER(${db.escape(
+      req.body.username
+    )});`,
+    (err, result) => {
+      if (result.length) {
+        return res.status(409).send({
+          msg: 'This username is already in use!',
+        });
+      } else {
+        // username is available
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).send({
+              msg: err,
+            });
+          } else {
+            // has hashed pw => add to database
+            db.query(
+              `INSERT INTO users (id, username, password, registered) VALUES ('${uuid.v4()}', ${db.escape(
+                req.body.username
+              )}, ${db.escape(hash)}, now())`,
+              (err, result) => {
+                if (err) {
+                  throw err;
+                  return res.status(400).send({
+                    msg: err,
+                  });
+                }
+                return res.status(201).send({
+                  msg: 'Registered!',
+                });
+              }
+            );
+          }
+        });
+      }
+    }
+  );
+});
